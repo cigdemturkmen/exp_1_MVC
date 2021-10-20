6fda5f2b6115df82ff52885e8907a6e6fafaeae4
Çiğdem Türkmen <cigdemturkmen57@outlook.com>
2021-10-20 14:01:55 +0300

    Add project files.

---
+++ exp_1_MVC/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace exp_1_MVC.Controllers
+{
+    public class HomeController : Controller  //Home - controllerin adı. home controllerın içindeki index'i çalıştırma.
+    {
+        // GET: Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ exp_1_MVC/Models/Ogrenci.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace exp_1_MVC.Models
+{
+    public class Ogrenci
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string StudentNumber { get; set; }
+    }
+}
